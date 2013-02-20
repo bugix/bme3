bfda0b89190cf528779d4abaa7e77a987471188b
Darshan <darshanm@SPEC.com>
2013-02-20 11:01:51 +0530

    AppLoader Changes.

--- bme2/src/main/java/medizin/client/ui/widget/process/AppLoader.java
+++ bme2/src/main/java/medizin/client/ui/widget/process/AppLoader.java
@@ -5,13 +5,32 @@ import medizin.client.factory.request.RequestEvent.Handler;
 
 import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.Timer;
 import com.google.inject.Inject;
 
 public class AppLoader {
 
 	private static int eventCounter = 0;
-
+	private static boolean showLoader = true;
+	private static final int LOADING_TIMEOUT = 50;
 	// private boolean show;
+	
+	private final Timer showTimer = new Timer() {
+	    @Override
+	    public void run() {
+	      if (eventCounter > 0) {
+	    	  ApplicationLoadingPopupView.showApplicationLoadingPopup(true);
+	      }
+	    }
+	};
+
+	public static boolean isShowLoader() {
+		return showLoader;
+	}
+
+	public static void setShowLoader(boolean showLoader) {
+		AppLoader.showLoader = showLoader;
+	}
 
 	public static void initialCounter() {
 		eventCounter = 0;
@@ -21,7 +40,8 @@ public class AppLoader {
 
 		if (show) {
 			eventCounter++;
-			ApplicationLoadingPopupView.showApplicationLoadingPopup(show);
+			showTimer.schedule(LOADING_TIMEOUT);
+			//ApplicationLoadingPopupView.showApplicationLoadingPopup(show);
 		} else {
 			eventCounter--;
 			if (eventCounter <= 0) {
@@ -40,27 +60,31 @@ public class AppLoader {
 			public void onRequestEvent(RequestEvent event) {
 
 				RequestEvent.State state = event.getState();
+				if (showLoader) {
+	
 
-				switch (state) {
-				case SENT: {
-					display(true);
-//					Log.info("state : SENT ");
-					break;
+					switch (state) {
+					case SENT: {
+						display(true);
+	//					Log.info("state : SENT ");
+						break;
+					}
+					case RECEIVED: {
+						display(false);
+	//					Log.info("state : RECEIVED ");
+						break;
+					}
+					default: {
+						display(false);
+						Log.info("state default or Invalid : Error ");
+						break;
+					}
+					}
+	
 				}
-				case RECEIVED: {
-					display(false);
-//					Log.info("state : RECEIVED ");
-					break;
-				}
-				default: {
-					display(false);
-					Log.info("state default or Invalid : Error ");
-					break;
-				}
-				}
-
 			}
 		});
 
 	}
+	
 }
