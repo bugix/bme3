a66c411c5f7dde3fbd6ead72ca85cdc60e916247
Tapan Thakkar <tapan.thakkar@spec-india.com>
2013-07-25 14:56:19 +0530

    Bug fix in rich text toolbar for formula.

--- bme2/src/main/java/medizin/client/ui/richtext/RichTextToolbar.java
+++ bme2/src/main/java/medizin/client/ui/richtext/RichTextToolbar.java
@@ -260,7 +260,9 @@ public class RichTextToolbar extends Composite {
 			
 			@Override
 			public void onClose(CloseEvent<PopupPanel> event) {
-				richText.getFormatter().insertHTML(dialogImpl.getEquation()); 	
+				if(dialogImpl.getEquation() != null && dialogImpl.getEquation().isEmpty() == false) {
+					richText.getFormatter().insertHTML(dialogImpl.getEquation());	
+				}
 			}
 		});
       }
